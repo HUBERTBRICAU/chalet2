4858633c89f4f6e8c916f14705b2fd2963204351
HUBERTBRICAU <73237744+HUBERTBRICAU@users.noreply.github.com>
2024-08-20 08:58:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+const basSwiper = new Swiper('.bas1', {
+  // Optional parameters
+  
+  loop: true,
+  slidesPerView:3,
+  slidesPerGroup:3,
+  spaceBeetween:3,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+const hautSwiper = new Swiper('.haut1', {
+    // Optional parameters
+    
+    // loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs :{
+      swiper: basSwiper
+    }
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+  // =================================================================
+  const bas2Swiper = new Swiper('.bas2', {
+    // Optional parameters
+    
+    loop: true,
+    slidesPerView:3,
+    slidesPerGroup:3,
+    spaceBeetween:3,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+  const hautSwiper2 = new Swiper('.haut2', {
+      // Optional parameters
+      
+      // loop: true,
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      thumbs :{
+        swiper: bas2Swiper
+      }
+    
+      // And if we need scrollbar
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      // },
+    });
+  
